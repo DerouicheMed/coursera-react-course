0da1ef71f98137d61d7cb9cb1e6a7e3ff0d6b5de
DerouicheMed <derouiche.mo7amed@gmail.com>
2020-07-19 23:22:08 +0100

    :package:components part1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
