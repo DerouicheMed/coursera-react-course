c22017eef993eb116dd400ce39f5c2cd124e5625
DerouicheMed <derouiche.mo7amed@gmail.com>
2020-08-02 16:05:20 +0100

    :package: Assignment 3

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -7,8 +7,122 @@ import {
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Label,
+  Row,
+  Col,
 } from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      isModalOpen: false,
+    };
+  }
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+  handleSubmit(values) {
+    alert("Form: " + JSON.stringify(values));
+  }
+  render() {
+    return (
+      <>
+        <Button outline color="secondary" onClick={this.toggleModal}>
+          <i class="fa fa-pencil-alt"></i>
+          Submit Comment
+        </Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <Row className="form-group">
+                <Label md={12}>Rating</Label>
+                <Col md={12}>
+                  <Control.select
+                    model=".rating"
+                    name="rating"
+                    className="form-control"
+                  >
+                    <option key="1">1</option>
+                    <option key="2">2</option>
+                    <option key="3">3</option>
+                    <option key="4">4</option>
+                    <option key="5" selected>
+                      5
+                    </option>
+                  </Control.select>
+                </Col>
+              </Row>
+
+              <Row className="form-group">
+                <Label md={12}>Your Name</Label>
+                <Col md={12}>
+                  <Control.text
+                    model=".name"
+                    id="name"
+                    name="name"
+                    placeholder="your Name"
+                    className="form-control"
+                    validators={{
+                      required,
+                      minLength: minLength(3),
+                      maxLength: maxLength(15),
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".name"
+                    show="touched"
+                    messages={{
+                      required: "Required ",
+                      minLength: "Must be at least 3 characters long ",
+                      maxLength: "Must be less than or equal to 15 characters ",
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Label md={12}>Comment</Label>
+                <Col md={12}>
+                  <Control.textarea
+                    model=".comment"
+                    id="comment"
+                    name="comment"
+                    rows="6"
+                    className="form-control"
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Col md={12}>
+                  <Button type="submit" color="primary">
+                    Submit
+                  </Button>
+                </Col>
+              </Row>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </>
+    );
+  }
+}
 
 const RenderComments = ({ comments }) => {
   return (
@@ -31,6 +145,7 @@ const RenderComments = ({ comments }) => {
           );
         })}
       </ul>
+      <CommentForm />
     </>
   );
 };
